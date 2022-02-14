980239314971cf91fcf0820c4127cd9599fd317e
sy1125 <96372212+sy1125@users.noreply.github.com>
2022-02-14 15:59:13 +0900

    Initial commit

---
+++ src/components/LottoBall.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="ball" :style="styleObject">{{ number }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LottoBall",
+
+  props: {
+    number: Number,
+  },
+
+  computed: {
+    styleObject() {
+      let background;
+      if (this.number <= 10) {
+        background = "red";
+      } else if (this.number <= 20) {
+        background = "orange";
+      } else if (this.number <= 30) {
+        background = "yellow";
+      } else if (this.number <= 40) {
+        background = "blue";
+      } else {
+        background = "green";
+      }
+      return {
+        background,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.ball {
+  display: inline-block;
+  border: 1px solid #000;
+  border-radius: 20px;
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+  font-size: 20px;
+  text-align: center;
+  margin-right: 20px;
+}
+</style>
